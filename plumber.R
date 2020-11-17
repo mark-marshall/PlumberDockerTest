5e63320178c732f90f88d5f6cfff484b7a5891ef
Mark Marshall <mark.marshallgp@gmail.com>
2020-11-17 10:25:52 +0000

    Added PNG endpoint

--- plumber.R
+++ plumber.R
@@ -6,3 +6,11 @@ library(openair)
 function(msg="") {
   list(msg = paste0("The message is: '", msg, "'"))
 }
+
+#* Plot a histogram
+#* @serializer png
+#* @get /polarplot
+function() {
+  data_mary <- importAURN(site = "my1", year = 2013, verbose = FALSE)
+  polarPlot(data_mary, pollutant = "no2", statistic = "mean")
+}
